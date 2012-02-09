6168df4bd79d05046c09a007b34b8ecf5e8ab1fd
Iftach Bar <iftachbar@gmail.com>
2012-02-09 17:59:39 +0200

    finished the client basic usability

--- main.py
+++ main.py
@@ -3,16 +3,42 @@ import tornado.web as web
 import tornadio2
 
 class MyConnection(tornadio2.SocketConnection):
-    def on_open(self, *args, **kwargs):
-        print "open"
-        self.emit("slide", {"slide_number": 2, "slide_header" : "My first cool slide"})
+    clients = set()
+    master = None
+    slide = None
+
+    def on_open(self, info):
+        self.clients.add(self)
+        self._send_current_slide()
 
     @tornadio2.event("like")
-    def on_like(self):
-        print "liked it!"
+    def on_like(self, slide):
+        if self.master is not None:
+            self.master.emit("like", slide)
+
+    @tornadio2.event("slide_transfer")
+    def on_slide_transfer(self):
+        if self is not self.master:
+            return
+
+        self.slide = {"slide_number": 2, "slide_header" : "My first cool slide"}
+
+        self._update_clients()
+
+    def _update_clients(self):
+        for c in self.clients:
+            c._send_current_slide()
+
+    def _send_current_slide(self):
+        self.emit("slide", self.slide)
 
     def on_close(self):
-        print "closed"
+        if self is self.master:
+            self.slide = None
+            self.master = None
+            self._update_clients()
+        else:
+            self.clients.remove(self)
 
 MyRouter = tornadio2.TornadioRouter(MyConnection)
 
