7c5dcd52d7ffab95d273eb8376c3f2869667d996
Iftach Bar <iftachbar@gmail.com>
2012-02-09 17:03:52 +0200

    Added a like button and slide event

--- main.py
+++ main.py
@@ -2,30 +2,27 @@ import tornado.ioloop
 import tornado.web as web
 import tornadio2
 
-class MainHandler(web.RequestHandler):
-	def get(self):
-		self.write("Hello, world")
-
 class MyConnection(tornadio2.SocketConnection):
+    def on_open(self, *args, **kwargs):
+        print "open"
+        self.emit("slide", {"slide_number": 2, "slide_header" : "My first cool slide"})
 
-	def on_open(self, *args, **kwargs):
-		print "open"
+    @tornadio2.event("like")
+    def on_like(self):
+        print "liked it!"
 
-	def on_message(self, message):
-		print "hi"
+    def on_close(self):
+        print "closed"
 
-	def on_close(self):
-		print "closed"
-	
 MyRouter = tornadio2.TornadioRouter(MyConnection)
 
 routes = [
-	(r"/static/(.*)", web.StaticFileHandler, {"path": "./static"}),
-	(r"/html/(.*)", web.StaticFileHandler, {"path": "./"})]
+    (r"/static/(.*)", web.StaticFileHandler, {"path": "./static"}),
+    (r"/html/(.*)", web.StaticFileHandler, {"path": "./"})]
 routes.extend(MyRouter.urls)
 
 application = web.Application(routes, socket_io_port = 8000)
 
 if __name__ == "__main__":
-	application.listen(8888)
-	tornado.ioloop.IOLoop.instance().start()
+    application.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
